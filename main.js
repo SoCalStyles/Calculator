71fda3880f9d444467750cecf26ea5e117d92e4d
SoCalStyles <stylesaaronk@gmail.com>
2022-09-20 12:07:45 -0700

    add buttons to html, create js variables for them

---
+++ main.js
@@ -0,0 +1,46 @@
+let displayValue = ' ';
+
+const oneButton = document.querySelector('#one');
+const twoButton = document.querySelector('#two');
+const threeButton = document.querySelector('#three');
+const fourButton = document.querySelector('#four');
+const fiveButton = document.querySelector('#five');
+const sixButton = document.querySelector('#six');
+const sevenButton = document.querySelector('#seven');
+const eightButton = document.querySelector('#eight');
+const nineButton = document.querySelector('#nine');
+const zeroButton = document.querySelector('#zero');
+
+const addButton = document.querySelector('#addButton');
+const subtractButton = document.querySelector('#subtractButton');
+const multiplyButton = document.querySelector('#multiplyButton');
+const divideButton = document.querySelector('#divideButton');
+
+const equalsButton = document.querySelector('#equalsButton');
+
+const clearButton = document.querySelector('#clearButton');
+
+
+displayValue += button
+
+const addition = function(args) {
+
+}
+
+const subtraction = function(args) {
+
+}
+
+const multiply = function(args) {
+    
+}
+
+const divide = function(args) {
+
+}
+
+const operate = function() {
+    // takes an operator and 2 numbers and then calls one of the 
+    // above functions on the numbers
+}
+
